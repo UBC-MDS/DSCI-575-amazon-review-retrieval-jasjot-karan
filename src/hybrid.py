4d4993c70a91b6f3f1e047e4d0e8b166bd2b7607
jasjotp <jasjotparmar14@gmail.com>
2026-04-14 20:55:21 -0700

    wire up tavily web search tool and create _should_use_web_search() function to add web search to relevant queries

--- src/hybrid.py
+++ src/hybrid.py
@@ -6,7 +6,6 @@ Rank Fusion (RRF).
 from collections import defaultdict
 from bm25 import bm25_search
 from semantic import semantic_search
-from tools import tavily_web_search
 
 def hybrid_search(
     query: str, 
